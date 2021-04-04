55be5181427d46577002a75e1fe4b440d49caf63
Titan Stark <add.sivakumar@gmail.com>
2021-04-04 15:56:54 +0530

    ToDo Project

--- ToDoApp/ToDoApp/wwwroot/js/site.js
+++ ToDoApp/ToDoApp/wwwroot/js/site.js
@@ -5,18 +5,10 @@
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-
-
 // Write your JavaScript code.
-
-
 
 const uri = 'http://localhost:59451/api/todo';
 let todolist = [];
-
-
-
-
 
 function getToDos() {
     fetch(uri, {
@@ -33,13 +25,11 @@ function getToDos() {
         .catch(error => console.error('Unable to get items.', error));
 }
 
-
-
 function addToDo() {
-  
+    debugger;
     const ToDo = {
-        Description: document.getElementById('Description').value,
-        IsDone: document.getElementById('IsDone').value
+        description: document.getElementById('description').value,
+        isDone: document.getElementById('isDone').value
     };
     //Fetch API
     //axios
@@ -63,8 +53,6 @@ function addToDo() {
         .catch(error => console.error('Unable to add item.', error));
 }
 
-
-
 function deleteItem(id) {
     fetch(`${uri}/${id}`, {
         method: 'DELETE'
@@ -73,31 +61,21 @@ function deleteItem(id) {
         .catch(error => console.error('Unable to delete item.', error));
 }
 
-
-
 function displayEditForm(id) {
-  
+    debugger;
     const item = todolist.find(item => item.Id === id);
-
-
-
-    document.getElementById('editId').value = item.Id;
-    document.getElementById('editDescription').value = item.Description;
-    document.getElementById('editIsDone').value = item.IsDone;
+    document.getElementById('editid').value = item.id;
+    document.getElementById('editdescription').value = item.description;
+    document.getElementById('editisDone').value = item.isDone;
     document.getElementById('editForm').style.display = 'block';
 }
 
-
-
 function updateItem() {
-    const Id = document.getElementById('editId').value;
+    const Id = document.getElementById('editid').value;
     const ToDo = {
-        Id: document.getElementById('editId').value,
-        Description: document.getElementById('editDescription').value,
-        IsDone: document.getElementById('editIsDone').value
+        Description: document.getElementById('editdescription').value,
+        IsDone: document.getElementById('editisDone').value
     };
-
-
 
     fetch(`${uri}/${Id}`, {
         method: 'PUT',
@@ -112,77 +90,45 @@ function updateItem() {
         .then(() => getToDos())
         .catch(error => console.error('Unable to update item.', error));
 
-
-
     closeInput();
-
-
-
     return false;
 }
-
-
 
 function closeInput() {
     document.getElementById('editForm').style.display = 'none';
 }
 
-
-
 function _displayCount(itemCount) {
     const name = (itemCount === 1) ? 'to-do' : 'to-dos';
-
-
-
     document.getElementById('counter').innerText = `${itemCount} ${name}`;
 }
 
-
-
 function _displaytodoList(data) {
- 
+    debugger;
     const tBody = document.getElementById('todolist');
     tBody.innerHTML = '';
-
-
-
     _displayCount(data.length);
-
-
-
     const button = document.createElement('button');
-
-
 
     data.forEach(item => {
         let lableforId = document.createElement('label');
-        lableforId.innerHTML = item.Id;
-
-
+        lableforId.innerHTML = item.id;
 
         let lableforDescription = document.createElement('label');
-        lableforDescription.innerHTML = item.Description;
-
-
+        lableforDescription.innerHTML = item.description;
 
         let lableforIsDone = document.createElement('label');
-        lableforIsDone.innerHTML = item.IsDone;
+        lableforIsDone.innerHTML = item.isDone;
 
         let editButton = button.cloneNode(false);
         editButton.innerText = 'Edit';
-        editButton.setAttribute('onclick', `displayEditForm(${item.Id})`);
-
-
+        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
 
         let deleteButton = button.cloneNode(false);
         deleteButton.innerText = 'Delete';
-        deleteButton.setAttribute('onclick', `deleteItem(${item.Id})`);
-
-
+        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
 
         let tr = tBody.insertRow();
-
-
 
         let td1 = tr.insertCell(0);
         td1.appendChild(lableforId);
@@ -190,28 +136,19 @@ function _displaytodoList(data) {
         let td2 = tr.insertCell(1);
         td2.appendChild(lableforDescription);
 
-
-
         let td3 = tr.insertCell(2);
         td3.appendChild(lableforIsDone);
-
 
         //let td2 = tr.insertCell(1);
         //let textNode = document.createTextNode(item.name);
         //td2.appendChild(textNode);
 
+        let td4 = tr.insertCell(3);
+        td4.appendChild(editButton);
 
-
-        let td5 = tr.insertCell(3);
-        td5.appendChild(editButton);
-
-
-
-        let td6 = tr.insertCell(4);
-        td6.appendChild(deleteButton);
+        let td5 = tr.insertCell(4);
+        td5.appendChild(deleteButton);
     });
-
-
 
     todolist = data;
 }
